9dd50afbaf508749d105549c1e3ee66538fe597c
Tora <tora@raum.network>
2025-03-02 16:14:09 +0530

    Updated Title of Website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Chrysalis",
+  title: "Chrysalis - SwiftStake",
   description: "Chrysalis By raum.network",
 };
 
